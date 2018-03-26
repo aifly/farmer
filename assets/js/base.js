c64afd150be7f57cf199fdf71a239d9e3ce9b30c
aifly <aifly_xc@163.com>
2018-03-26 12:11:47 +0800

    a

--- assets/js/base.js
+++ assets/js/base.js
@@ -2,32 +2,6 @@ var imgs = {
 	
 
 }
-
-var arr = [];
-for (var attr in imgs) {
-	arr.push(imgs[attr]);
-}
-var loading = function(arr, fn, fnEnd) {
-	var len = arr.length;
-	var count = 0;
-	var i = 0;
-
-	function loadimg() {
-		if (i === len) {
-			return;
-		}
-		var img = new Image();
-		img.onload = img.onerror = function() {
-			count++;
-			if (i < len - 1) {
-				i++;
-				loadimg();
-				fn && fn(i / (len - 1), img.src);
-			} else {
-				fnEnd && fnEnd(img.src);
-			}
-		};
-		img.src = arr[i];
-	}
-	loadimg();
+window.config = {
+	baseUrl:'https://api.symbin.cn/v1'
 }
--- components/login/index.vue
+++ components/login/index.vue
@@ -1,15 +1,15 @@
 <template>
 	<div class="symbin-login-ui">
-		login222222
-		<p>111111</p>
-
-		<Icon type="checkmark" />
+		<Input v-model="username" placeholder="请输入用户名"></Input>	
+		<Input type='password' v-model="password" placeholder="请输入用密码" :clearable='true'></Input>
+		<Button type="primary" @click='login'>登录</Button>
 	</div>
 </template>
 
 <script>
 	import './index.css';
 	import $ from 'jquery';
+	import symbinUtil from '../lib/util';
 	import Vue from "vue";
 
 	export default {
@@ -17,14 +17,27 @@
 		name:'zmitiindex',
 		data(){
 			return{
-				
+				username:'',
+				password:''
 			}
 		},
 		components:{
 		},
 		
 		methods:{
-			
+			login(){
+				var _this = this;
+				symbinUtil.ajax({
+					url:window.config.baseUrl+'/admin/adminlogin',
+					data:{
+						adminusername:_this.username,
+						adminpwd:_this.password
+					},
+					fn(data){
+						console.log(data);
+					}
+				})
+			}
 
 		},
 		mounted(){
