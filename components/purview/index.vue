14797e2f940153a999cc188bc3d8f6de612adf63
clearinfo <songxian@163.com>
2018-04-08 12:25:39 +0800

    song

--- components/purview/index.vue
+++ components/purview/index.vue
@@ -4,7 +4,7 @@
 			<Row>
 		        <Col span="6"><h5>权限管理</h5></Col>
 		        <Col span="18">
-		        	<div class="text-right"><Button type="primary" @click="modal1 = true">新增权限</Button></div>
+		        	<div class="text-right"><Button type="primary" @click="open">新增权限</Button></div>
 		        </Col>
 		    </Row>
 		</div>
@@ -110,7 +110,7 @@
 	    	</div>
 	    </Modal>
 		<div class="top20"></div>
-		<Table :columns="columns1" :data="data1"></Table>
+		<Table :columns="columns1" :data="listData"></Table>
 
 	</div>
 </template>
@@ -128,6 +128,7 @@
 		data(){
 			return {
 				modal1: false,
+				currentIndex:'',
 				parentList: [
                     {
                         value: '1',
@@ -170,13 +171,41 @@
                     comment:'',
                 },
 				columns1: [
-                    {
-                        title: 'Name',
-                        key: 'name'
+					{
+                        title: '权限ID',
+                        key: 'actionid'
                     },
                     {
-                        title: 'Age',
-                        key: 'age'
+                        title: '父级权限ID',
+                        key: 'parentactionid'
+                    },
+                    {
+                        title: '中文名称',
+                        key: 'actionname'
+                    },
+                    {
+                        title: '英文名称',
+                        key: 'englishname'
+                    },
+                    {
+                        title: '权限编号',
+                        key: 'actionnumber'
+                    },
+                    {
+                        title: '权限级别',
+                        key: 'urllevel'
+                    },
+                    {
+                        title: '权限地址',
+                        key: 'actionurl'
+                    },
+                    {
+                    	title:'创建时间',
+                    	key:'createtime',
+                    },
+                    {
+                        title: '状态',
+                        key: 'status'
                     },
                     {
                         title: 'Action',
@@ -195,10 +224,11 @@
                                     },
                                     on: {
                                         click: () => {
-                                            this.show(params.index)
+                                            this.show(params.index);
+                                            console.log(params,'params');
                                         }
                                     }
-                                }, 'View'),
+                                }, '修改'),
                                 h('Button', {
                                     props: {
                                         type: 'error',
@@ -206,36 +236,16 @@
                                     },
                                     on: {
                                         click: () => {
-                                            this.remove(params.index)
+                                            this.remove(params.row.actionid);
+                                            
                                         }
                                     }
-                                }, 'Delete')
+                                }, '删除')
                             ]);
                         }
                     }
                 ],
-                data1: [
-                    {
-                        name: 'John Brown',
-                        age: 18,
-                        date: '2016-10-01'
-                    },
-                    {
-                        name: 'Jim Green',
-                        age: 24,
-                        date: '2016-10-01'
-                    },
-                    {
-                        name: 'Joe Black',
-                        age: 30,
-                        date: '2016-10-02'
-                    },
-                    {
-                        name: 'Jon Snow',
-                        age: 26,
-                        date: '2016-10-04'
-                    }
-                ]
+                listData: []
 			}
 		},
 		components:{
@@ -247,30 +257,30 @@
 
 		},
 		methods:{
-			getaddColumns(){//增加新栏目
+			add(currentIndex){//增加新权限
 				var s = this;
+				var formparams={
+					actionname: s.formItem.actionname,
+                    englishname:s.formItem.englishname,
+                    parentactionid:s.formItem.parentactionid,
+                    actionurl:s.formItem.actionurl,
+                    actionnumber:s.formItem.actionnumber,
+                    urllevel:s.formItem.urllevel,
+                    sort:s.formItem.sort,
+                    isparent:s.formItem.isparent,
+                    isdefaultauth:s.formItem.isdefaultauth,
+                    keyword:s.formItem.keyword,
+                    showwhere:s.formItem.showwhere,
+                    menuid:s.formItem.menuid,
+                    comment:s.formItem.comment,
+				}
 				symbinUtil.ajax({
 					url:window.config.baseUrl+"/admin/adduserauthurl",
 					validate:s.validateData,
-					data:{
-						actionname: s.formItem.actionname,
-	                    englishname:s.formItem.englishname,
-	                    parentactionid:s.formItem.parentactionid,
-	                    actionurl:s.formItem.actionurl,
-	                    actionnumber:s.formItem.actionnumber,
-	                    urllevel:s.formItem.urllevel,
-	                    sort:s.formItem.sort,
-	                    isparent:s.formItem.isparent,
-	                    isdefaultauth:s.formItem.isdefaultauth,
-	                    keyword:s.formItem.keyword,
-	                    showwhere:s.formItem.showwhere,
-	                    menuid:s.formItem.menuid,
-	                    comment:s.formItem.comment,
-					},
+					data:formparams,
 					fn(data){
 						if(data.getret === 0){
 							s.$Message.success(data.getmsg);
-							//s.bingAgain();//显示栏目列表
 						}
 						else{
 							s.$Message.error({
@@ -282,17 +292,193 @@
 				})				
 
 			},
+			open(){
+				var s = this;
+				s.modal1 = true;
+                s.formItem.actionname='';
+                s.formItem.englishname='';
+                s.formItem.parentactionid='';
+                s.formItem.actionurl='';
+                s.formItem.actionnumber='';
+                s.formItem.urllevel='';
+                s.formItem.sort='';
+                s.formItem.isparent='';
+                s.formItem.isdefaultauth='';
+                s.formItem.keyword='';
+                s.formItem.showwhere='';
+                s.formItem.menuid='';
+                s.formItem.comment='';
+			},
 			ok () {
-				this.getaddColumns();
+				var s = this;
+				//this.add();
                 //this.$Message.info('Clicked ok');
+                if(s.currentIndex!=''){
+
+                }else{
+                	this.add();
+                }
 
             },
             cancel () {
                 this.$Message.info('Clicked cancel');
-            }
+            },
+            custom () {
+                this.$Modal.confirm({
+                    title: '删除权限',
+                    content: '<p>Content of dialog</p><p>Content of dialog</p>',
+                    okText: '确定',
+                    cancelText: '取消'
+                });
+            },
+            getColumnslist(rak,fn){//获取数据
+				var s=this;
+				symbinUtil.ajax({
+					url:window.config.baseUrl+"/admin/getauthllist",
+					type:'post',
+					validate:s.validateData,
+					data:{
+						actionid:s.actionid,
+						parentactionid:s.parentactionid,
+						actionname:s.actionname,
+						englishname:s.englishname,
+						actionnumber:s.actionnumber,
+						urllevel:s.urllevel,
+						actionurl:s.actionurl,
+						createuserid:s.createuserid,
+						createtime:s.createtime,
+						updatauserid:s.updatauserid,
+						updatatime:s.updatatime,
+						sort:s.sort,
+						isparent:s.isparent,
+						isdefaultauth:s.isdefaultauth,
+						showwhere:s.showwhere,
+						menuid:s.menuid,
+						status:s.status,
+					},
+					fn(data){
+						
+						if(data.getret===0){
+							console.log(data,'data');
+							s.listData=data.list;
+						}
+						else{
+
+							 s.$Message.error({
+							  	content:data.getmsg,
+							  	duration: 10
+							  });
+							 if(data.getret === 1300){
+							 	window.location.hash = '/login/'
+
+							 }
+						}
+						
+					}
+				})
+			},
+			show(index){//查看
+				var s = this;
+				s.modal1=true;
+				s.currentIndex=this.listData[index].actionid;
+				console.log(s.currentIndex,'s.currentIndex');
+				s.formItem.actionname=s.listData[index].actionname;
+				s.formItem.englishname=s.listData[index].englishname;
+				s.formItem.parentactionid=s.listData[index].parentactionid;
+				s.formItem.actionurl=s.listData[index].actionurl;
+				s.formItem.actionnumber=s.listData[index].actionnumber;
+				s.formItem.urllevel=s.listData[index].urllevel;
+				s.formItem.sort=s.listData[index].sort;
+				s.formItem.isparent=s.listData[index].isparent;
+				s.formItem.isdefaultauth=s.listData[index].isdefaultauth;
+				s.formItem.keyword=s.listData[index].keyword;
+				s.formItem.showwhere=s.listData[index].showwhere;
+				s.formItem.menuid=s.listData[index].menuid;
+				s.formItem.comment=s.listData[index].comment;
+				/*var html='';
+				html+="<div class=symbin-purview-det>";
+				html+=`权限id：${this.listData[index].actionid}`;
+				html+=`<br>中文名称：${this.listData[index].actionname}`;
+				html+=`<br>英文名称：${this.listData[index].englishname}`;
+				html+=`<br>父级id${this.listData[index].parentactionid}`;
+				html+=`<br>url地址：${this.listData[index].actionurl}`;
+				html+=`<br>所属作品：${this.listData[index].worksid}`;
+				html+=`<br>权限编号：${this.listData[index].actionnumber}`;
+				html+=`<br>权限等级：${this.listData[index].urllevel}`;
+				html+=`<br>排序：${this.listData[index].sort}`;
+				html+=`<br>是否一级权限：${this.listData[index].isparent}`;
+				html+=`<br>关键词：${this.listData[index].keyword}`;
+				html+=`<br>栏目编号：${this.listData[index].menuid}`;
+				html+=`<br>备注：${this.listData[index].comment}`;
+				html+="</div>";*/
+				/*
+				this.$Modal.info({
+                    title: '查看权限',
+                    content: html
+                })*/
+			},
+			edit(actionid){
+				var s = this;
+				var formparams={
+					actionid:actionid,
+					actionname: s.formItem.actionname,
+                    englishname:s.formItem.englishname,
+                    parentactionid:s.formItem.parentactionid,
+                    actionurl:s.formItem.actionurl,
+                    actionnumber:s.formItem.actionnumber,
+                    urllevel:s.formItem.urllevel,
+                    sort:s.formItem.sort,
+                    isparent:s.formItem.isparent,
+                    isdefaultauth:s.formItem.isdefaultauth,
+                    keyword:s.formItem.keyword,
+                    showwhere:s.formItem.showwhere,
+                    menuid:s.formItem.menuid,
+                    comment:s.formItem.comment,
+				}
+				symbinUtil.ajax({
+					url:window.config.baseUrl+"/admin/edituserauthurl",
+					validate:s.validateData,
+					data:formparams,
+					fn(data){
+						if(data.getret === 0){
+							s.$Message.success(data.getmsg);
+						}
+						else{
+							s.$Message.error({
+								content:data.getmsg,
+								duration: 10
+							});
+						}
+					}
+				})
+			},
+			remove(actionids,deltype){//删除
+				var s = this;
+				symbinUtil.ajax({
+					url:window.config.baseUrl+"/admin/deluserauthurl",
+					type:'post',
+					validate:s.validateData,
+					data:{
+						actionids:actionids,
+						deltype:1,
+					},
+					fn(data){						
+						if(data.getret===0){
+							console.log(data,'data');
+							s.listData=data.list;
+						}
+						else{
+							s.$Message.error({
+								content:data.getmsg,
+								duration: 10
+							});
+						}						
+					}
+				})
+			}			
 		},
 		mounted(){//页面加载完成后显示
-
+			this.getColumnslist();//获取列表
 			
 		},
 	}
